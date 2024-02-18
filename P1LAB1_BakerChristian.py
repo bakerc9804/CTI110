d701408b94112b95b4c2773ebb0433476a068f7d
bakerc9804 <160412565+bakerc9804@users.noreply.github.com>
2024-02-18 10:38:42 -0500

    Add files via upload
    First submission.

---
+++ P1LAB1_BakerChristian.py
@@ -0,0 +1,7 @@
+# Christian Baker
+# February 18th, 2024
+# P1LAB1
+# Write a program that takes a first name as the input, and outputs a welcome message to that name.
+name = input("Enter your name:")
+print('Hey', name)
+print("You have completed your first assignment!")
