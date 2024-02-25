c6cb980655c7542564f7ebc3ea171700f07438bc
bakerc9804 <160412565+bakerc9804@users.noreply.github.com>
2024-02-25 09:54:36 -0500

    Add files via upload
    Submission 1 of 2 on February 25th, 2024.

---
+++ P1HW1_BakerChristian.py
@@ -0,0 +1,18 @@
+# Christian Baker
+# February 25th, 2024
+# P1HW1
+# Using Python's input and print functions, create a program similar to LAB: Warm up: Basic output with variables
+x = int(input('Enter integer:')) 
+print('You entered:', + int(x))
+number_sqrt = x**2
+print(x, 'squared is', number_sqrt)
+number_cubed = x**3
+print('And', x, 'cubed is', number_cubed, '!!')
+y = int(input('Enter another integer:'))
+sum = x + y
+print(x, '+', y, 'is', sum)
+product = x*y
+print(x, '*', y, 'is', product)
+
+
+
